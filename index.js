a74548d773525a1d07375588953f569c95a2740d
Max Khalzov <max.khalzov@gmail.com>
2018-10-04 19:44:29 +0300

    add first example

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var five = require("johnny-five");
-var board = new five.Board();
+var Raspi = require("raspi-io");
+var board = new five.Board({
+	io: new Raspi();
+});
 
 board.on("ready", function() {
+
   var lcd = new five.LCD({
-    // LCD pin name RS EN DB4 DB5 DB6 DB7
-    ping: [];
+    // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+    pins: ['GPIO25', 'GPIO24', 'GPIO23', 'GPIO17', 'GPIO27', 'GPIO22'],
   });
+
   var frame = 1;
-  var flames = [":runninga:", ":runningb:"];
+  var frames = [":runninga:", ":runningb:"];
   var row = 0;
   var col = 0;
 
+  // These calls will store the "runninga" and "runningb"
+  // characters in the LCD's built-in memory. The LCD
+  // allows up to 8 custom characters to be pre-loaded
+  // into memory.
+  //
+  // http://johnny-five.io/api/lcd/#predefined-characters
+  //
+  lcd.useChar("runninga");
+  lcd.useChar("runningb");
 
+  this.loop(300, function() {
+    lcd.clear().cursor(row, col).print(
+      frames[frame ^= 1]
+    );
 
+    if (++col === lcd.cols) {
+      col = 0;
+      if (++row === lcd.rows) {
+        row = 0;
+      }
+    }
+  });
 });
+

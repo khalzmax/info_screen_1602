9499e6e9dd6768db5b389fd4499efa3f309e1756
Max Khalzov (rpi_info_screen) <max.khalzov@gmail.com>
2018-10-05 19:37:09 +0000

    various fixed; now stable

--- index.js
+++ index.js
@@ -10,58 +10,63 @@ board.on("ready", function() {
     // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
     pins: ['GPIO25', 'GPIO24', 'GPIO23', 'GPIO17', 'GPIO27', 'GPIO22'],
   });
-  var multi = new five.Multi({
-    controller: "BMP180"
-  });
 
   // init widgets
   var runner = Runner(lcd);
-  var sensor = Sensor_bmp080(lcd, multi);
+  var sensor = Sensor_bmp080(lcd);
 
   var widgets = [ runner, sensor ];
+
   var currentWidget = 0;
+
+  widgets[currentWidget].run();
   var presentationInterval = setInterval(() => {
-   widgets[currentWidget].stop();
-   if (++currentWidget > widgets.length) {
-    currentWidget = 0;
-   }
-   widgets[currentWidget].run();
-  }, 5000);
+    widgets[currentWidget].stop();
+    if (++currentWidget >= widgets.length) {
+        currentWidget = 0;
+    }
+    widgets[currentWidget].run();
+  }, 10500);
 
   var stopPresentatoin = () => {
     clearInterval(presentationInterval);
   }
   this.repl.inject({
-      lcd, runner, sensor, stopPresentatoin
+    lcd, runner, sensor, stopPresentatoin
   });
 
 });
-function Sensor_bmp080(lcd, multi) {
+
+
+function Sensor_bmp080(lcd) {
+  var multi = new five.Multi({
+    controller: "BMP180"
+  });
   var temp, pressure, altitute;
   multi.on("change", function() {
-    temp = this.thermometer.celsius;
-    pressure = this.barometer.pressure;
-    altitute = this.altimeter.meters;
+    temp = this.thermometer.celsius.toFixed(0);
+    pressure = this.barometer.pressure.toFixed(0);
+    altitute = this.altimeter.meters.toFixed(0);
   });
 
   var stopFlag;
     var next = function() {
-        lcd.clear();
-        lcd.print("temp & pressure");
-        lcd.cursor(1, 0);
-        lcd.print(`${temp} ${pressure}`);
+      lcd.clear();
+      lcd.print("Temp   Pressure");
+      lcd.cursor(1, 0);
+      lcd.print(`${temp} C   ${pressure} Pa`);
     }
     return {
         stop() {
-            stopFlag = true;
+          stopFlag = true;
         },
         run() {
           stopFlag = false;
           return board.loop(1000, function(stopLoop) {
-              if (stopFlag) {
-                stopLoop();
-                return;
-              }
+            if (stopFlag) {
+              stopLoop();
+              return;
+            }
             next();
           });
         }
@@ -87,9 +92,6 @@ function Runner(lcd) {
 
   var stopFlag;
   var next = function() {
-      if (stopFlag) {
-        return;
-      };
 
       lcd.clear().cursor(row, col).print(
         frames[frame ^= 1]
